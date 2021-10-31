fd1508468c8bed8a6fe3291f9f6a654421431886
Jared Krinke <jared.krinke@gmail.com>
2021-10-30 20:55:26 -0700

    Ambitious set of tests

---
+++ test/md2blog.spec.js
@@ -0,0 +1,72 @@
+import { describe, it } from "mocha";
+import assert from "assert";
+
+describe("md2blog", () => {
+    describe("Command line arguments", () => {
+        it("Input directory", () => {
+        });
+    
+        it("Output directory", () => {
+        });
+
+        it("Clean output directory", () => {
+        });
+
+        it("Exclude drafts", () => {
+        });
+    });
+
+    describe("Static assets", () => {
+    });
+
+    describe("Relative links", () => {
+    });
+
+    describe("Site metadata", () => {
+    });
+
+    describe("Arbitrary pages", () => {
+    });
+
+    describe("Posts", () => {
+        describe("Uncategorized posts", () => {
+        });
+
+        describe("Implicit categorization", () => {
+        });
+
+        describe("Keywords", () => {
+        });
+
+        describe("Tag navigation", () => {
+        });
+
+        describe("Syntax highlighting", () => {
+        });
+
+        describe("Broken links", () => {
+        });
+    });
+
+    describe("Tag indexes", () => {
+    });
+
+    describe("Index page", () => {
+        describe("Top tags", () => {
+        });
+    });
+
+    describe("News archive", () => {
+    });
+
+    describe("News feed", () => {
+        describe("Absolute links", () => {
+        });
+
+        describe("Relative links", () => {
+        });
+    });
+
+    describe("Error page", () => {
+    });
+});
