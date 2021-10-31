29ccfd6dfa15928d3790e82d9f1dd83dd2da650e
Jared Krinke <jared.krinke@gmail.com>
2021-10-30 21:53:03 -0700

    Trivial site test

--- test/md2blog.spec.js
+++ test/md2blog.spec.js
@@ -1,18 +1,44 @@
-import { describe, it } from "mocha";
 import assert from "assert";
+import { exec } from "child_process";
+import { promises } from "fs";
+import { describe, it } from "mocha";
+import { promisify } from "util";
+
+const execAsync = promisify(exec);
+
+const validateSuccessAsync = async (execPromise) => {
+    await assert.doesNotReject(execPromise);
+};
+
+const testAsync = async (workingDirectory, commandLineArgumentsString, validate = validateSuccessAsync) => {
+    const pathToModuleRoot = "../".repeat(Array.from(workingDirectory.matchAll(/[\\/]/g)).length + 1);
+    const commandLine = `node ${pathToModuleRoot}main.js ${commandLineArgumentsString}`;
+    await validate(execAsync(commandLine, {
+        cwd: workingDirectory,
+        windowsHide: true,
+        timeout: 10 * 1000, // ms
+    }));
+
+    // TODO: Actually delete the output directory?
+};
+
 
 describe("md2blog", () => {
+    describe("Trivial site", () => {
+        it("Builds successfully", async () => testAsync("test/data/trivial-site", ""));
+    });
+
     describe("Command line arguments", () => {
-        it("Input directory", () => {
+        it("Input directory", async () => {
         });
     
-        it("Output directory", () => {
+        it("Output directory", async () => {
         });
 
-        it("Clean output directory", () => {
+        it("Clean output directory", async () => {
         });
 
-        it("Exclude drafts", () => {
+        it("Exclude drafts", async () => {
         });
     });
 
