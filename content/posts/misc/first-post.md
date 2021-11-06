f67fa562b8d4f3807a49e094b93fb8ffebdb0976
Jared Krinke <jared.krinke@gmail.com>
2021-11-06 09:57:18 -0700

    File enumeration

---
+++ content/posts/misc/first-post.md
@@ -0,0 +1,6 @@
+---
+title: First post!
+date: 2021-11-06
+---
+# Hello, world!
+It's good to be here.
--- main.ts
+++ main.ts
@@ -1,1 +1,33 @@
-console.log("That's enough for today...");
+import { join } from "https://deno.land/std@0.113.0/path/mod.ts";
+
+const input = "content";
+// const output = "out";
+
+// type File = {
+//     path: string,
+//     data: Uint8Array,
+//     [propertyName: string]: any,
+// };
+
+async function enumerateFiles(directoryName: string): Promise<string[]> {
+    const filePaths: string[] = [];
+    for await (const dirEntry of Deno.readDir(directoryName)) {
+        const path = join(directoryName, dirEntry.name);
+        if (dirEntry.isFile) {
+            filePaths.push(path);
+        } else if (dirEntry.isDirectory) {
+            filePaths.push(...(await enumerateFiles(path)));
+        }
+    }
+    return filePaths;
+}
+
+console.log(await enumerateFiles(input));
+
+// async function readDirectoryEntry(path: string, dirEntry: Deno.DirEntry) {
+
+// }
+
+// async function readDirectory(path: string, directoryName: string) {
+//     for await (const dirEntry of Deno.readDir(join(input)))
+// }
