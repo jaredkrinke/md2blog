336a06f63a5ed1632a193e17f0197845b746f644
Jared Krinke <jared.krinke@gmail.com>
2021-11-09 08:31:53 -0800

    Root template

--- main.ts
+++ main.ts
@@ -310,40 +310,82 @@ function goldsmithLayoutLitesTemplar(options: GoldsmithLitesTemplarOptions): Gol
 const postPathPattern = /^posts(\/([^/]+))?\/[^/]+.md$/;
 
 // TODO: Move and fill in
-const partialBase = (m: Metadata, mainVerbatim: string) => 
+const partialBase = (m: Metadata, mainVerbatim: string, navigationVerbatim?: string) => 
 html`<!DOCTYPE html>
 <html lang="en">
 <head>
 <meta charset="utf-8" />
 <title>${m.site.title}${m.title ? `: ${m.title}` : ""}</title>
-${/*${m.#if description}<meta name="description" content="${m.description}" />${m./if}
-${m.#if keywords}<meta name="keywords" content="${m.#each keywords}${m.this}${m.#unless @last},${m./unless}${m./each}" />${m./if}*/""}
+${{verbatim: m.description ? html`<meta name="description" content="${m.description}" />` : ""}}
+${{verbatim: m.keywords ? html`<meta name="keywords" content="${m.keywords.join(",")}" />` : ""}}
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 <link rel="stylesheet" href="${m.rootPath}css/style.css" />
-${/*${m.#if isRoot}<link rel="alternate" type="application/rss+xml" href="${m.rootPath}feed.xml" />${m./if}*/""}
+${{verbatim: m.isRoot ? html`<link rel="alternate" type="application/rss+xml" href="${m.rootPath}feed.xml" />` : ""}}
 </head>
 <body>
 <header>
 <h1><a href="${m.rootPath}index.html">${m.site.title}</a></h1>
-${/*${m.site.description}<p>${m.site.description}</p>${m./if}*/""}
-${/*${m.#if navigationPartialName}${m.> (lookup . "navigationPartialName") }${m./if}*/""}
+${{verbatim: m.site.description ? html`<p>${m.site.description}</p>` : ""}}
+${{verbatim: navigationVerbatim ? navigationVerbatim : ""}}
 </header>
 <main>
 ${{verbatim: mainVerbatim}}
 </main>
 </body>
-</html>`;
+</html>
+`;
+
+function partialNavigation(m: Metadata, tags: string[], incomplete: boolean, isTagIndex?: boolean, tag?: string): string {
+    return tags ? html`<nav>
+<ul>
+${{verbatim: tags.map(t => (isTagIndex && t === tag) ? html`<li>${tag}</li>` : html`<li><a href="${m.rootPath}posts/${t}/index.html">${t}</a></li>`).join("\n")}}
+${{verbatim: incomplete ? html`<li><a href="${m.rootPath}posts/index.html">&hellip;</a></li>` : ""}}
+</ul>
+</nav>` : "";
+}
+
+const dateFormatter = new Intl.DateTimeFormat("en-US", { month: "long", day: "numeric", year: "numeric", timeZone: "UTC" });
+const formatDateShort = (date: Date) => date.toISOString().replace(/T.*$/, "");
+const formatDate = (date: Date) => dateFormatter.format(date);
+function partialDate(date: Date): string {
+    return html`<p><time datetime="${formatDateShort(date)}">${formatDate(date)}</time></p>`;
+}
+
+const partialArticleSummary: (m: Metadata, post: Metadata) => string = (m, post) => {
+    return html`<article>
+<header>
+<h1><a href="${m.linkPrefix}${post.path}">${post.title}</a></h1>
+${{verbatim: partialDate(post.date)}}
+</header>
+${{verbatim: post.description ? html`<p>${post.description}</p>` : ""}}
+</article>
+`;
+};
 
 const templateNotFound: GoldsmithLitesTemplarLayoutCallback = (_content, m) => partialBase(m,
 `<h1>Not found</h1>
 <p>The requested page does not exist.</p>
-<p><a href="index.html">Click here</a> to go to the home page.</p>`)
+<p><a href="index.html">Click here</a> to go to the home page.</p>
+`);
+
+const templateRoot: GoldsmithLitesTemplarLayoutCallback = (_content, m) => partialBase({
+    isRoot: true,
+    description: m.site.description,
+    ...m,
+},
+html`<ul>
+${{verbatim: m.posts_recent.map((post: Metadata) => html`<li>${{verbatim: partialArticleSummary(m, post)}}</li>`).join("\n")}}
+</ul>
+<footer>
+<p><a href="posts/index.html">See all articles</a> or subscribe to the <a href="feed.xml">Atom feed</a></p>
+</footer>`,
+partialNavigation(m, m.tagsTop, m.tagsTop.length !== m.tagsAll.length));
 
 const trivialLayout: GoldsmithLitesTemplarLayoutCallback = (source, metadata) => `{${Object.keys(metadata).join(", ")}}\n${source}`;
 const templates: GoldsmithLitesTemplarLayoutMap = {
     "post": trivialLayout,
     "tagIndex": trivialLayout,
-    "index": trivialLayout,
+    "index": templateRoot,
     "archive": trivialLayout,
     "404": templateNotFound,
     "default": trivialLayout,
@@ -461,5 +503,4 @@ await Goldsmith()
             defaultTemplate: "default",
         })
     }))
-    .use(goldsmithLog)
     .build();
