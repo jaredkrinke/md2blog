eb593ea8eb2d62dff5c492f3893c08404e87c7cf
Jared Krinke <jared.krinke@gmail.com>
2021-11-10 14:01:32 -0800

    Color library

---
+++ colorsmith.test.ts
@@ -0,0 +1,53 @@
+import { assertEquals } from "https://deno.land/std@0.113.0/testing/asserts.ts";
+import { hexToRGB, rgbToHex, rgbToHSL, hslToRGB } from "./colorsmith.ts";
+
+Deno.test({
+    name: "RGB to hex",
+    fn: () => {
+        assertEquals(rgbToHex({ r: 0.5, g: 0.5, b: 0.5 }), "#808080");
+        // assertEquals(rgbToHex(hslToRGB(rgbToHSL(hexToRGB(color)))), color);
+    },
+});
+
+Deno.test({
+    name: "Hex to RGB",
+    fn: () => {
+        assertEquals(hexToRGB("#808080"), { r: 0.5, g: 0.5, b: 0.5 });
+        // assertEquals(rgbToHex(hslToRGB(rgbToHSL(hexToRGB(color)))), color);
+    },
+});
+
+Deno.test({
+    name: "Round-trip hex through RGB",
+    fn: () => {
+        const color = "#123456";
+        assertEquals(rgbToHex(hexToRGB(color)), color);
+        // assertEquals(rgbToHex(hslToRGB(rgbToHSL(hexToRGB(color)))), color);
+    },
+});
+
+Deno.test({
+    name: "RGB to HSL",
+    fn: () => {
+        assertEquals(rgbToHSL({ r: 1, g: 0, b: 0}), { h: 0, s: 1, l: 0.5});
+        assertEquals(rgbToHSL({ r: 0, g: 1, b: 1}), { h: 180, s: 1, l: 0.5});
+    },
+});
+
+Deno.test({
+    name: "Round-trip hex through RGB and HSL",
+    fn: () => {
+        const colors = [
+            "#123456",
+            "#654321",
+            "#808080",
+            "#baf00d",
+            "#999999",
+            "#f1f2f3",
+        ];
+
+        for (const color of colors) {
+            assertEquals(rgbToHex(hslToRGB(rgbToHSL(hexToRGB(color)))), color);
+        }
+    },
+});
---
+++ colorsmith.ts
@@ -0,0 +1,55 @@
+// Color conversions
+/** Represents a color in red, green, and blue (all 0 - 1.0). */
+export type RGB = { r: number, g: number, b: number };
+
+/** Represents a color using hue (0 - 360 degrees), saturation (0 - 1.0), and lightness (0 - 1.0). */
+export type HSL = { h: number, s: number, l: number };
+
+const max = 256;
+
+/** Converts a 6 digit hex color of the form "#RRGGBB" to RGB (0 - 1.0). Note: 3 digit form ("#123") is not supported.  */
+export function hexToRGB(hex: string): RGB {
+    const r = parseInt(hex.substr(1, 2), 16) / max;
+    const g = parseInt(hex.substr(3, 2), 16) / max;
+    const b = parseInt(hex.substr(5, 2), 16) / max;
+    return { r, g, b};
+}
+
+export function rgbToHex(rgb: RGB): string {
+    const { r, g, b } = rgb;
+    return "#" + [r, g, b]
+        .map(x => Math.min(max - 1, Math.floor(x * max)).toString(16))
+        .map(str => str.length === 1 ? "0" + str : str)
+        .join("");
+}
+
+export function rgbToHSL(rgb: RGB): HSL {
+    const { r, g, b} = rgb;
+    const v = Math.max(r, g, b);
+    const c = Math.max(r, g, b) - Math.min(r, g, b);
+    const l = v - c / 2;
+    const h = 60 * ((c === 0) ? 0
+        : ((v === r) ? ((g - b) / c)
+        : ((v === g) ? (2 + (b - r) / c)
+        : (4 + (r - g) / c)
+    )));
+
+    const s = (l === 0 || l === 1) ? 0 : (c / (1 - Math.abs(2 * v - c - 1)));
+
+    return { h, s, l };
+}
+
+export function hslToRGB(hsl: HSL): RGB {
+    const { h, s, l } = hsl;
+    const f: (n: number) => number = (n) => {
+        const k = (n + h / 30) % 12;
+        const a = s * Math.min(l, 1 - l);
+        return l - a * Math.max(-1, Math.min(k - 3, 9 - k, 1));
+    }
+
+    return {
+        r: f(0),
+        g: f(8),
+        b: f(4),
+    }
+}
