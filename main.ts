d6bc0bd1c052651712fd3137740e1e52aabeb029
Jared Krinke <jared.krinke@gmail.com>
2021-11-10 17:24:53 -0800

    Themes

--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@ import HighlightJS from "https://jspm.dev/highlight.js@11.3.1";
 import { marked, Renderer } from "https://jspm.dev/marked@4.0.0";
 import { html, xml } from "../lites-templar/mod.ts";
 import { cheerio, Root, Cheerio } from "https://deno.land/x/cheerio@1.0.4/mod.ts";
+import { hexToRGB, rgbToHSL, hslToRGB, rgbToHex } from "./colorsmith.ts";
 
 // TODO: Types from JSPM don't work
 // deno-lint-ignore no-explicit-any
@@ -363,6 +364,7 @@ function pathRelativeResolve(from: string, to: string): string {
     }
     return currentPath.slice(1);
 }
+
 // TODO: Make other plugins functions for future extensibility without breaking change
 const relativeLinkPattern = /^[^/][^:]*$/;
 function goldsmithBrokenLinkChecker(): Plugin {
@@ -660,268 +662,135 @@ await Goldsmith()
     }))
     .use(goldsmithInjectFiles({
         "css/style.css": {
-            data: `:root {
-  color-scheme: dark;
-}
-html,
+            data: (metadata: Metadata) => {
+                let css = `:root { color-scheme: dark; }
+html, body { margin: 0; }
+
 body {
-  margin: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-family: sans-serif;
+    overflow-y: scroll;
 }
-body {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  font-family: sans-serif;
-  overflow-y: scroll;
-}
-body > header,
-main {
-  width: 40em;
-}
-body > header,
-main {
-  padding: 0.5em;
-}
+
+body > header, main { width: 40em; }
+body > header, main { padding: 0.5em; }
+
 @media screen and (max-width: 40em) {
-  body > header,
-  main {
-    width: calc(100% - 1em);
-  }
+    body > header, main { width: calc(100% - 1em); }
 }
-body {
-  line-height: 1.5;
-}
-pre code {
-  line-height: 1.3;
-}
-h1,
-h2,
-h3,
-h4,
-h5 {
-  line-height: 1.2;
-}
-td {
-  line-height: 1.2;
-}
-body > header {
-  text-align: center;
-}
-body > header > h1 {
-  margin-bottom: 0.25em;
-}
-body > header > p {
-  margin-top: 0.25em;
-}
-nav {
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-}
-nav > ul {
-  margin: 0;
-  padding: 0 0 0 0.5em;
-  display: inline;
-}
-nav > ul > li {
-  display: inline;
-}
-nav > ul > li:first-child:before {
-  content: "Topics: ";
-}
-nav > ul > li + li:before {
-  content: " | ";
-}
+
+body { line-height: 1.5; }
+pre code { line-height: 1.3; }
+h1, h2, h3, h4, h5 { line-height: 1.2; }
+td { line-height: 1.2; }
+
+body > header { text-align: center; }
+body > header > h1 { margin-bottom: 0.25em; }
+body > header > p { margin-top: 0.25em; }
+
+nav { display: flex; flex-direction: row; justify-content: center; }
+nav > ul { margin: 0; padding: 0 0 0 0.5em; display: inline; }
+nav > ul > li { display: inline; }
+nav > ul > li:first-child:before { content: "Topics: "; }
+nav > ul > li + li:before { content: " | "; }
+
 body > header > h1 > a {
-  font-size: 1.75rem;
-  text-decoration: inherit;
+    font-size: 1.75rem;
+    text-decoration: inherit;
 }
-article > header > h1 {
-  margin-bottom: 0.25em;
-}
-article > header > p {
-  margin-top: 0em;
-}
-main footer {
-  margin-top: 1em;
-}
-main {
-  overflow: auto;
-}
-pre {
-  overflow: auto;
-}
-article img,
-article svg {
-  display: block;
-}
+
+article > header > h1 { margin-bottom: 0.25em; }
+article > header > p { margin-top: 0em; }
+main footer { margin-top: 1em; }
+
+main { overflow: auto; }
+pre { overflow: auto; }
+article img, article svg { display: block; }
+
+code { font-size: 1rem; }
+pre code { font-size: 0.8125rem; }
+
+th, td { padding: 0.25em; }
+pre { border: solid 1px; padding: 0em 0.25em; }
+table, th, tr, td { border-collapse: collapse; border: solid 1px; }
+
+main > ul { padding-left: 0em; }
+main > ul > li { list-style: none; margin-bottom: 2em; }
+
+ul { padding-left: 1.5em; }
+li { margin-bottom: 0.5em; }
+
+h1 { font-size: 1.6rem; font-weight: bold; }
+h2 { font-size: 1.3rem; font-weight: bold; }
+h3 { font-size: 1.2rem; font-weight: normal; }
+h4 { font-size: 1rem; font-weight: bold; }
+h5 { font-size: 1rem; font-weight: normal; }
+
+:is(h1, h2, h3, h4, h5) :is(a:link, a:visited) { color: inherit; }
+
+body { background-color: @background; }
+* { color: @textDefault; }
+main footer { border-top: 1px solid @backgroundLightest; }
+th { background-color: @backgroundLightest; color: @textLight; }
+pre, table, th, tr, td { border-color: @border; }
+pre { background-color: @backgroundLighter; }
+tr:nth-child(even) { background-color: @backgroundLight; }
+
 code {
-  font-size: 1rem;
+    background-color: @backgroundEvenLighter;
+    border-radius: 0.2em;
+    padding: 0em 0.1em;
 }
+
 pre code {
-  font-size: 0.8125rem;
+    background-color: revert;
+    border-radius: revert;
+    padding: revert;
 }
-th,
-td {
-  padding: 0.25em;
-}
-pre {
-  border: solid 1px;
-  padding: 0em 0.25em;
-}
-table,
-th,
-tr,
-td {
-  border-collapse: collapse;
-  border: solid 1px;
-}
-main > ul {
-  padding-left: 0em;
-}
-main > ul > li {
-  list-style: none;
-  margin-bottom: 2em;
-}
-ul {
-  padding-left: 1.5em;
-}
-li {
-  margin-bottom: 0.5em;
-}
-h1 {
-  font-size: 1.6rem;
-  font-weight: bold;
-}
-h2 {
-  font-size: 1.3rem;
-  font-weight: bold;
-}
-h3 {
-  font-size: 1.2rem;
-  font-weight: normal;
-}
-h4 {
-  font-size: 1rem;
-  font-weight: bold;
-}
-h5 {
-  font-size: 1rem;
-  font-weight: normal;
-}
-:is(h1, h2, h3, h4, h5) :is(a:link, a:visited) {
-  color: inherit;
-}
-body {
-  background-color: #181818;
-}
-* {
-  color: #c8c8c8;
-}
-main footer {
-  border-top: 1px solid #4b4b4b;
-}
-th {
-  background-color: #4b4b4b;
-  color: #dcdcdc;
-}
-pre,
-table,
-th,
-tr,
-td {
-  border-color: #5f5f5f;
-}
-pre {
-  background-color: #2c2c2c;
-}
-tr:nth-child(even) {
-  background-color: #222222;
-}
-code {
-  background-color: #373737;
-  border-radius: 0.2em;
-  padding: 0em 0.1em;
-}
-pre code {
-  background-color: revert;
-  border-radius: revert;
-  padding: revert;
-}
-body > header > h1 {
-  color: #ffffff;
-}
-nav > ul > li:first-child:before {
-  font-weight: bold;
-  color: #ff80ff;
-}
-h1,
-h2,
-h3,
-h4,
-h5 {
-  color: #ff80ff;
-}
-a:link {
-  color: #55ffff;
-}
-a:visited {
-  color: #4af0f0;
-}
+
+body > header > h1 { color: @textTitle; }
+nav > ul > li:first-child:before { font-weight: bold; color: @textHeading; }
+h1, h2, h3, h4, h5 { color: @textHeading; }
+a:link { color: @textLink; }
+a:visited { color: @textLinkVisited; }
 /* Also b5cea8 or 7dce52 or 7bbf56 */
+
 /* Diagrams */
-svg text {
-  fill: #dcdcdc;
-}
-.diagram-transparent-white {
-  stroke: none;
-  fill: none;
-}
-ellipse.diagram-black-none {
-  stroke: #a2a2a2;
-  fill: #373737;
-}
-.diagram-black-none {
-  stroke: #a2a2a2;
-  fill: none;
-}
-.diagram-black-black {
-  stroke: #a2a2a2;
-  fill: #2c2c2c;
-}
+svg text { fill: @textLight; }
+.diagram-transparent-white { stroke: none; fill: none; }
+ellipse.diagram-black-none { stroke: @textDark; fill: @backgroundEvenLighter; }
+.diagram-black-none { stroke: @textDark; fill:none; }
+.diagram-black-black { stroke: @textDark; fill: @backgroundLighter; }
+
 /* Syntax highlighting */
-.hljs-comment {
-  color: #41c741;
-}
+.hljs-comment { color: @textCommentDark; }
+
 .hljs-tag,
-.hljs-punctuation {
-  color: #a2a2a2;
-}
-.hljs-literal {
-  color: #4af0f0;
-}
+.hljs-punctuation { color: @textDark; }
+
+.hljs-literal { color: @textLinkVisited; }
+
 .hljs-title.class_,
 .hljs-tag .hljs-name,
-.hljs-tag .hljs-attr {
-  color: #4d4;
-}
+.hljs-tag .hljs-attr { color: @textComment; }
+
 .hljs-attr,
 .hljs-symbol,
 .hljs-variable,
 .hljs-template-variable,
 .hljs-link,
 .hljs-selector-attr,
-.hljs-selector-pseudo {
-  color: #55ffff;
-}
+.hljs-selector-pseudo { color: @textLink; }
+
 .hljs-keyword,
 .hljs-attribute,
 .hljs-selector-tag,
 .hljs-meta .hljs-keyword,
 .hljs-doctag,
-.hljs-name {
-  color: #4af0f0;
-}
+.hljs-name { color: @textLinkVisited; }
+
 .hljs-type,
 .hljs-string,
 .hljs-number,
@@ -930,45 +799,78 @@ ellipse.diagram-black-none {
 .hljs-deletion,
 .hljs-title,
 .hljs-section,
-.hljs-meta {
-  color: #ff80ff;
-}
+.hljs-meta { color: @textHeading; }
+
 .hljs-regexp,
-.hljs-meta .hljs-string {
-  color: #f472f4;
-}
+.hljs-meta .hljs-string { color: @textHeadingDark; }
+
 .hljs-title.function_,
 .hljs-built_in,
 .hljs-bullet,
 .hljs-code,
 .hljs-addition,
 .hljs-selector-id,
-.hljs-selector-class {
-  color: #ffffff;
-}
-`
-            // // TODO: Custom colors
-            // data: () => {
-            //     // let source = (await promises.readFile(path.join(moduleStaticFromCWD, "css", "style.less"))).toString();
+.hljs-selector-class { color: @textTitle; }
+`;
+                
+                // Base colors
+                const textTitle = metadata?.site?.colors?.title ?? "#e6b95c";
+                const textHeading = metadata?.site?.colors?.heading ?? "#d97c57";
+                const textLink = metadata?.site?.colors?.link ?? "#59c5ff";
+                const textComment = metadata?.site?.colors?.comment ?? "#7bbf56";
+                const textDefault = "#c8c8c8";
+                const background = "#181818";
 
-            //     // // Override default colors, if custom colors provided
-            //     // const customColors = metadata?.site?.colors ?? {};
-            //     // const colorRegExp = /^(#[0-9a-fA-F]{3,6})|([a-z]{3,30})$/;
-            //     // for (const mapping of [
-            //     //     { key: "title", variable: "textTitle" },
-            //     //     { key: "heading", variable: "textHeading" },
-            //     //     { key: "link", variable: "textLink" },
-            //     //     { key: "comment", variable: "textComment" },
-            //     // ]) {
-            //     //     const value = customColors[mapping.key];
-            //     //     if (value && colorRegExp.test(value)) {
-            //     //         source = source.replace(new RegExp(`[@]${mapping.variable}:[^;]*;`), `@${mapping.variable}: ${value};`);
-            //     //     }
-            //     // }
+                let colors: { [name: string]: string } = {
+                    textTitle,
+                    textHeading,
+                    textLink,
+                    textComment,
+                    textDefault,
+                    background,
+                };
 
-            //     // const output = await less.render(source);
-            //     // return output.css;
-            // }
+                // Derived colors
+                const desaturateStep = -0.15;
+                const darkenStep = -0.05;
+                const lightenStep = 0.04;
+
+                function adjust(hex: string, h?: number, s?: number, l?: number): string {
+                    const hsl = rgbToHSL(hexToRGB(hex));
+                    if (h) {
+                        hsl.h = (hsl.h + h) % 360;
+                    }
+                    if (s) {
+                        hsl.s = Math.min(1, Math.max(0, hsl.s + s));
+                    }
+                    if (l) {
+                        hsl.l = Math.min(1, Math.max(0, hsl.l + l));
+                    }
+                    return rgbToHex(hslToRGB(hsl));
+                }
+
+                colors = {
+                    ...colors,
+                    textDark: adjust(textDefault, 0, 0, -0.15),
+                    textLight: adjust(textDefault, 0, 0, lightenStep * 2),
+                    textHeadingDark: adjust(textHeading, 0, desaturateStep, darkenStep),
+                    textLinkVisited: adjust(textLink, 0, desaturateStep, darkenStep),
+                    textCommentDark: adjust(textComment, 0, desaturateStep, darkenStep),
+                    backgroundLight: adjust(background, 0, 0, lightenStep),
+                    backgroundLighter: adjust(background, 0, 0, lightenStep * 2),
+                    backgroundEvenLighter: adjust(background, 0, 0, lightenStep * 3),
+                    backgroundLightest: adjust(background, 0, 0, lightenStep * 5),
+                    border: adjust(background, 0, 0, lightenStep * 7),
+                }
+
+                // Find and replace
+                for (const colorName of Object.keys(colors)) {
+                    css = css.replaceAll(`@${colorName};`, `${colors[colorName]};`);
+                }
+
+                const textEncoder = new TextEncoder();
+                return textEncoder.encode(css);
+            }
         },
     }))
     .use(goldsmithMarked({
@@ -990,7 +892,6 @@ ellipse.diagram-black-none {
         })
     }))
     .use(goldsmithBrokenLinkChecker())
-    // TODO: Broken link checker
     // TODO: Local web server with automatic reloading
     // TODO: Only for testing
     .use((files) => {
