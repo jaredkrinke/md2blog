8085e0c7912a291824311bebd80fed1cccf589a0
Jared Krinke <jared.krinke@gmail.com>
2021-11-24 15:23:15 -0800

    Build script

--- build.bat
+++ build.bat
@@ -1,1 +1,2 @@
-deno compile -o md2blog.exe --allow-net=localhost --allow-read --allow-write ./main.ts
+deno compile --target x86_64-pc-windows-msvc -o md2blog --allow-net=localhost --allow-read --allow-write ./main.ts
+deno compile --target x86_64-unknown-linux-gnu -o md2blog --allow-net=localhost --allow-read --allow-write ./main.ts
