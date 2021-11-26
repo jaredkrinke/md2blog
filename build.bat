5311066be862801f1a05836dd88fe34808377989
Jared Krinke <jared.krinke@gmail.com>
2021-11-26 13:32:45 -0800

    Zip binaries

--- build.bat
+++ build.bat
@@ -1,2 +1,5 @@
 deno compile --target x86_64-pc-windows-msvc -o md2blog --allow-net=localhost --allow-read --allow-write ./main.ts
+powershell Compress-Archive -Path md2blog.exe -DestinationPath md2blog-x86_64-pc-windows-msvc.zip -Force
+
 deno compile --target x86_64-unknown-linux-gnu -o md2blog --allow-net=localhost --allow-read --allow-write ./main.ts
+powershell Compress-Archive -Path md2blog -DestinationPath md2blog-x86_64-unknown-linux-gnu.zip -Force
