90516f3f2e1e3f465b7250ad965007331e79c6fd
Jared Krinke <jared.krinke@gmail.com>
2021-11-30 12:46:27 -0800

    macOS builds

--- build.bat
+++ build.bat
@@ -3,3 +3,9 @@ powershell Compress-Archive -Path md2blog.exe -DestinationPath md2blog-x86_64-pc
 
 deno compile --target x86_64-unknown-linux-gnu -o md2blog --allow-net=localhost --allow-read --allow-write ./main.ts
 powershell Compress-Archive -Path md2blog -DestinationPath md2blog-x86_64-unknown-linux-gnu.zip -Force
+
+deno compile --target x86_64-apple-darwin -o md2blog --allow-net=localhost --allow-read --allow-write ./main.ts
+powershell Compress-Archive -Path md2blog -DestinationPath md2blog-x86_64-apple-darwin.zip -Force
+
+deno compile --target aarch64-apple-darwin -o md2blog --allow-net=localhost --allow-read --allow-write ./main.ts
+powershell Compress-Archive -Path md2blog -DestinationPath md2blog-aarch64-apple-darwin.zip -Force
