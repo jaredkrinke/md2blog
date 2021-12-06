a9cf13bf62a51894dc24d088be83afa5281b3c78
Jared Krinke <jared.krinke@gmail.com>
2021-12-05 20:44:19 -0800

    Use published tool

--- schema/build.bat
+++ schema/build.bat
@@ -1,2 +1,2 @@
-deno run --allow-read=site.schema.json --allow-write=site.ts ../../json-schema-aot/main.ts site.schema.json --ts site.ts
-deno run --allow-read=post.schema.json --allow-write=post.ts ../../json-schema-aot/main.ts post.schema.json --ts post.ts
+deno run --allow-read=site.schema.json --allow-write=site.ts https://deno.land/x/json_schema_aot@0.2.0/main.ts site.schema.json --ts site.ts
+deno run --allow-read=post.schema.json --allow-write=post.ts https://deno.land/x/json_schema_aot@0.2.0/main.ts post.schema.json --ts post.ts
